2eded0150fdbffb7eeedb2849db923cbfe4a8cb1
tackyunicorn <joshjoseph97@gmail.com>
2019-04-17 11:30:43 +0530

    :card_file_box: querying documents

--- mongodb.js
+++ mongodb.js
@@ -5,10 +5,6 @@ const { MongoClient, ObjectID } = require('mongodb')
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-const id = new ObjectID()
-console.log(id.id.length)
-console.log(id.toHexString().length)
-
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
 	if (error) {
 		return console.log('Unable to connect to database!')
@@ -16,53 +12,27 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
 	const db = client.db(databaseName)
 
-	// db.collection('users').insertOne({
-	// 	_id: id,
-	// 	name: 'Vikram',
-	// 	age: 21
-	// }, (error, result) => {
+	// db.collection('users').findOne({ _id: new ObjectID("5cb6b2ef1274ef121088709b") }, (error, user) => {
 	// 	if (error) {
-	// 		return console.log('Unable to insert user!')
+	// 		return console.log('Unable to fetch user!')
 	// 	}
 
-	// 	console.log(result.ops)
+	// 	console.log(user)
 	// })
 
-	// db.collection('users').insertMany([
-	// 	{
-	// 		name: 'Joshua',
-	// 		age: 21
-	// 	},
-	// 	{
-	// 		name: 'Jen',
-	// 		age: 23
-	// 	}
-	// ], (error, result) => {
-	// 	if (error) {
-	// 		return console.log('Unable to insert documents!')
-	// 	}
-
-	// 	console.log(result.ops)
+	// db.collection('users').find({ age: 21 }).toArray((error, users) => {
+	// 	console.log(users)
 	// })
 
-	// db.collection('tasks').insertMany([
-	// 	{
-	// 		description: 'Book tickets',
-	// 		completed: true
-	// 	},
-	// 	{
-	// 		description: 'Wash dishes',
-	// 		completed: true
-	// 	},
-	// 	{
-	// 		description: 'Study for internals',
-	// 		completed: false
-	// 	}
-	// ], (error, result) => {
-	// 	if (error) {
-	// 		return console.log('Unable to insert documents!')
-	// 	}
+	db.collection('tasks').findOne({ _id: new ObjectID("5cb6ae399790f90804a0fc3b") }, (error, task) => {
+		if (error) {
+			return console.log('Unable to fetch task')
+		}
 
-	// 	console.log(result.ops)
-	// })
+		console.log(task)
+	})
+
+	db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+		console.log(tasks)
+	})
 })
