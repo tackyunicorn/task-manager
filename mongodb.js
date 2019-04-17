03098dbe28d0e8adea3bbf0bbd945bcce8f13703
tackyunicorn <joshjoseph97@gmail.com>
2019-04-17 11:02:16 +0530

    :card_file_box: object id

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,13 @@
 // CRUD create read update delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectID()
+console.log(id.id.length)
+console.log(id.toHexString().length)
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
 	if (error) {
@@ -14,7 +17,8 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 	const db = client.db(databaseName)
 
 	// db.collection('users').insertOne({
-	// 	name: "Joshua",
+	// 	_id: id,
+	// 	name: 'Vikram',
 	// 	age: 21
 	// }, (error, result) => {
 	// 	if (error) {
@@ -41,24 +45,24 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 	// 	console.log(result.ops)
 	// })
 
-	db.collection('tasks').insertMany([
-		{
-			description: 'Book tickets',
-			completed: true
-		},
-		{
-			description: 'Wash dishes',
-			completed: true
-		},
-		{
-			description: 'Study for internals',
-			completed: false
-		}
-	], (error, result) => {
-		if (error) {
-			return console.log('Unable to insert documents!')
-		}
+	// db.collection('tasks').insertMany([
+	// 	{
+	// 		description: 'Book tickets',
+	// 		completed: true
+	// 	},
+	// 	{
+	// 		description: 'Wash dishes',
+	// 		completed: true
+	// 	},
+	// 	{
+	// 		description: 'Study for internals',
+	// 		completed: false
+	// 	}
+	// ], (error, result) => {
+	// 	if (error) {
+	// 		return console.log('Unable to insert documents!')
+	// 	}
 
-		console.log(result.ops)
-	})
+	// 	console.log(result.ops)
+	// })
 })
