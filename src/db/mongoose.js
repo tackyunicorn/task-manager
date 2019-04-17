701686e10153d5e853fac5498d65f6ec4a3cb232
tackyunicorn <joshjoseph97@gmail.com>
2019-04-17 16:18:24 +0530

    :card_file_box: data validation and sanitization one

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 	useNewUrlParser: true,
@@ -7,10 +8,29 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
 	name: {
-		type: String
+		type: String,
+		required: true,
+		trim: true
+	},
+	email: {
+		type: 'String',
+		required: true,
+		trim: true,
+		lowercase: true,
+		validate(value) {
+			if (!validator.isEmail(value)) {
+				throw new Error('Email is invalid!')
+			}
+		}
 	},
 	age: {
-		type: Number
+		type: Number,
+		default: 0,
+		validate(value) {
+			if (value < 0) {
+				throw new Error('Age must be a positive number')
+			}
+		}
 	}
 })
 
@@ -24,23 +44,23 @@ const Task = mongoose.model('Task', {
 })
 
 const me = new User({
-	name: 'Joshua',
-	age: 'Mike'
+	name: '     Joshua     ',
+	email: 'JOSHJOSEPH97@GMAIL.COM'
 })
 
-const task = new Task({
-	description: 'Clean the house',
-	completed: false
-})
-
-// me.save().then(() => {
-// 	console.log(me)
-// }).catch((error) => {
-// 	console.log('Error! ', error)
+// const task = new Task({
+// 	description: 'Clean the house',
+// 	completed: false
 // })
 
-task.save().then((task) => {
-	console.log(task)
+me.save().then(() => {
+	console.log(me)
 }).catch((error) => {
 	console.log('Error! ', error)
 })
+
+// task.save().then((task) => {
+// 	console.log(task)
+// }).catch((error) => {
+// 	console.log('Error! ', error)
+// })
